b63b325d8dbc3e3547203991afcae4cdafa16a65
Hawen <hawen.1307@gmail.com>
2024-10-16 18:01:06 +0300

    Added JS logic with some CSS and HTML improvments

---
+++ script.js
@@ -0,0 +1,79 @@
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()-=_+[]{}|;:,.<>?/';
+
+class PasswordSpecs {
+    constructor() {
+        this.caseVariant = document.querySelector('input[name="case"]:checked').value;
+        this.passwordLength = document.getElementById("range__length").value;
+        this.includeNumbers = document.getElementById("checkbox__numbers").checked;
+        this.includeSymbols = document.getElementById("checkbox__symbols").checked;
+        this.includeLetters = document.getElementById("checkbox__letters").checked;
+    }
+}
+
+function changeRangeValue() {
+    let value = document.getElementById("range__length").value;
+    document.getElementById("range__value").innerText = value
+}
+
+function switchCaseDisplay() {
+    if (document.getElementById("checkbox__letters").checked) {
+        document.getElementsByClassName("main-section_case")[0].style.display = "flex";
+    } else {
+        document.getElementsByClassName("main-section_case")[0].style.display = "none";
+    }
+}
+
+function generatePassword() {
+    const passwordSpecs = new PasswordSpecs;
+
+    let len = passwordSpecs.passwordLength;
+    let caseVariant = passwordSpecs.caseVariant;
+    let includeLetters = passwordSpecs.includeLetters;
+    let includeNumbers = passwordSpecs.includeNumbers;
+    let includeSymbols = passwordSpecs.includeSymbols;
+    
+    let chars = '';
+    let password = '';
+
+    if (includeLetters) {
+        switch (caseVariant) {
+            case "mixed":
+                chars += uppercase;
+                chars += lowercase;
+                break;
+            
+            case "lowercase":
+                chars += lowercase;
+                break;
+
+            case "uppercase":
+                chars += uppercase;
+                break;
+
+            default:
+                break;
+        }
+    }
+    if (includeNumbers) {
+        chars += numbers;
+    }
+    if (includeSymbols) {
+        chars += symbols;
+    }
+
+    const allCharsLen = chars.length;
+
+    for (let i = 0; i < len; i++) {
+        const randomIndex = Math.floor(window.crypto.getRandomValues(new Uint32Array(1))[0] / (0xFFFFFFFF + 1) * allCharsLen);
+        password += chars.charAt(randomIndex);
+    }
+
+    document.getElementById("output").value = password
+}
+
+if (!window.crypto && !window.crypto.getRandomValues) {
+    alert("Упс! Кажется ваш браузер не поддерживает Crypto API. Пароль не может быть сгенерирован.")
+}
